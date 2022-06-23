be9141daf0e89712f763d3b219f1ee4ba4d788fa
hsjeong <nicewook@hotmail.com>
2022-06-23 16:59:50 +0900

    remove: ch1 related codes

--- dswg/cmd/server/ch1test.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/zsh -v
-curl -X POST localhost:8080 -d '{"record":{"value":"1234"}}'
-curl -X POST localhost:8080 -d '{"record":{"value":"5678"}}'
-curl -X POST localhost:8080 -d '{"record":{"value":"90AB"}}'
-curl -X GET localhost:8080 -d '{"offset":0}'
-curl -X GET localhost:8080 -d '{"offset":1}'
-curl -X GET localhost:8080 -d '{"offset":2}'
