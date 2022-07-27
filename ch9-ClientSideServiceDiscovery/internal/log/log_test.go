db2d34627831215e1b489113980176a08f974c38
hsjeong <nicewook@hotmail.com>
2022-07-27 10:06:22 +0900

    ch9 complete

---
+++ ch9-ClientSideServiceDiscovery/internal/log/log_test.go
@@ -0,0 +1,142 @@
+package log
+
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+
+	api "github.com/nicewook/proglog/api/v1"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/protobuf/proto"
+)
+
+func TestLog(t *testing.T) {
+	for scenario, fn := range map[string]func(
+		t *testing.T, log *Log,
+	){
+		"append and read a record succeeds": testAppendRead,
+		"offset out of range error":         testOutOfRangeErr,
+		"init with existing segments":       testInitExisting,
+		"reader":                            testReader,
+		"truncate":                          testTruncate,
+		"make new segment":                  testNewSegment,
+	} {
+		t.Run(scenario, func(t *testing.T) {
+			dir, err := ioutil.TempDir("", "store-test")
+			require.NoError(t, err)
+			defer os.RemoveAll(dir)
+
+			c := Config{}
+			c.Segment.MaxStoreBytes = 32
+			if scenario == "make new segment" {
+				c.Segment.MaxIndexBytes = 13 // only one offset:position pair
+			}
+			log, err := NewLog(dir, c)
+			require.NoError(t, err)
+
+			fn(t, log)
+		})
+	}
+}
+
+func testAppendRead(t *testing.T, log *Log) {
+	append := &api.Record{
+		Value: []byte("hello world"),
+	}
+	off, err := log.Append(append)
+	require.NoError(t, err)
+	require.Equal(t, uint64(0), off)
+
+	read, err := log.Read(off)
+	require.NoError(t, err)
+	require.Equal(t, append.Value, read.Value)
+}
+
+func testOutOfRangeErr(t *testing.T, log *Log) {
+	// read, err := log.Read(1)
+	// require.Nil(t, read)
+	// require.Error(t, err)
+
+	read, err := log.Read(1)
+	require.Nil(t, read)
+	apiErr := err.(api.ErrOffsetOutOfRange)
+	require.Equal(t, uint64(1), apiErr.Offset)
+
+}
+
+func testInitExisting(t *testing.T, o *Log) {
+	append := &api.Record{
+		Value: []byte("hello world"),
+	}
+	for i := 0; i < 3; i++ {
+		_, err := o.Append(append)
+		require.NoError(t, err)
+	}
+	require.NoError(t, o.Close())
+
+	off, err := o.LowestOffset()
+	require.NoError(t, err)
+	require.Equal(t, uint64(0), off)
+	off, err = o.HighestOffset()
+	require.NoError(t, err)
+	require.Equal(t, uint64(2), off)
+
+	n, err := NewLog(o.Dir, o.Config)
+	require.NoError(t, err)
+
+	off, err = n.LowestOffset()
+	require.NoError(t, err)
+	require.Equal(t, uint64(0), off)
+	off, err = n.HighestOffset()
+	require.NoError(t, err)
+	require.Equal(t, uint64(2), off)
+}
+
+func testReader(t *testing.T, log *Log) {
+	append := &api.Record{
+		Value: []byte("hello world"),
+	}
+	off, err := log.Append(append)
+	require.NoError(t, err)
+	require.Equal(t, uint64(0), off)
+
+	reader := log.Reader()
+	b, err := ioutil.ReadAll(reader)
+	require.NoError(t, err)
+
+	read := &api.Record{}
+	err = proto.Unmarshal(b[lenWidth:], read)
+	require.NoError(t, err)
+	require.Equal(t, append.Value, read.Value)
+}
+
+func testTruncate(t *testing.T, log *Log) {
+	append := &api.Record{
+		Value: []byte("hello world"),
+	}
+	for i := 0; i < 3; i++ {
+		_, err := log.Append(append)
+		require.NoError(t, err)
+	}
+
+	_, err := log.Read(0)
+	require.NoError(t, err)
+
+	err = log.Truncate(1)
+	require.NoError(t, err)
+
+	_, err = log.Read(0)
+	require.Error(t, err)
+}
+
+func testNewSegment(t *testing.T, log *Log) {
+	append := &api.Record{
+		Value: []byte("hello world"),
+	}
+	for i := 0; i < 3; i++ {
+		_, err := log.Append(append)
+		require.NoError(t, err)
+	}
+
+	require.Equal(t, 3, len(log.segments))
+}
---
+++ ch9-ClientSideServiceDiscovery/internal/log/store_test.go
@@ -0,0 +1,108 @@
+package log
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+var (
+	write = []byte("hello world")
+	width = uint64(len(write)) + lenWidth
+)
+
+func TeststoreAppendRead(t *testing.T) {
+	f, err := os.CreateTemp("", "store_append_read_test")
+	require.NoError(t, err)
+	defer os.Remove(f.Name())
+
+	s, err := newStore(f)
+	require.NoError(t, err)
+
+	// 추가하고 읽기 테스트
+	testAppend(t, s)
+	testRead(t, s)
+	testReadAt(t, s)
+
+	// 다시 읽어도 같은 파일을 가리키는 store를 다시 가져오는 것 확인
+	s, err = newStore(f)
+	require.NoError(t, err)
+	testRead(t, s)
+}
+
+func testAppend(t *testing.T, s *store) {
+	t.Helper()
+	for i := uint64(1); i < 4; i++ {
+		n, pos, err := s.Append(write)
+		require.NoError(t, err)
+		require.Equal(t, pos+n, width*i) // pos에서 n만큼 추가했으니 width*i
+	}
+}
+
+func testRead(t *testing.T, s *store) {
+	t.Helper()
+	var pos uint64
+	for i := uint64(1); i < 4; i++ { // i가 uint64일 필요는 없구나
+		read, err := s.Read(pos) // pos 위치를 읽는다.size 읽고 그 사이즈 만큼 읽고
+		require.NoError(t, err)
+		require.Equal(t, write, read)
+		pos += width
+	}
+}
+
+func testReadAt(t *testing.T, s *store) {
+	t.Helper()
+	for i, off := uint64(1), int64(0); i < 4; i++ {
+		b := make([]byte, lenWidth)
+		n, err := s.ReadAt(b, off)
+		require.NoError(t, err)
+		require.Equal(t, lenWidth, n)
+		off += int64(n)
+
+		size := enc.Uint64(b)
+		b = make([]byte, size)
+		n, err = s.ReadAt(b, off)
+		require.NoError(t, err)
+		require.Equal(t, write, b)
+		require.Equal(t, int(size), n) // 실제로 사이즈만큼 읽었는가?
+		off += int64(n)
+	}
+}
+
+func TestStoreClose(t *testing.T) {
+	f, err := os.CreateTemp("", "store_close_test")
+	require.NoError(t, err)
+	defer os.Remove(f.Name())
+	s, err := newStore(f)
+	require.NoError(t, err)
+	_, _, err = s.Append(write)
+	require.NoError(t, err)
+
+	_, beforeSize, err := openFile(f.Name())
+	require.NoError(t, err)
+
+	err = s.Close() //
+	require.NoError(t, err)
+
+	_, afterSize, err := openFile(f.Name())
+	require.NoError(t, err)
+	require.True(t, afterSize > beforeSize)
+	t.Logf("beforeSize %d, afterSzie %d", beforeSize, afterSize)
+}
+
+func openFile(name string) (file *os.File, size int64, err error) {
+	f, err := os.OpenFile(
+		name,
+		os.O_RDWR|os.O_CREATE|os.O_APPEND,
+		0644,
+	)
+	if err != nil {
+		return nil, 0, err
+	}
+	fi, err := f.Stat()
+	if err != nil {
+		return nil, 0, err
+	}
+	return f, fi.Size(), nil
+}
